75b75ced388958ef2990b6d7e6054ef3e441a245
Kodak2001 <kuba99p@gmail.com>
2021-03-23 22:04:10 +0100

    makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+game: main.o fun_main.o gifenc.o
+	cc main.c fun_main.c gifenc.c -o game
+	rm *.o
+	
--- zapis.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZAPIS_H
-#define ZAPIS_H
-
-#include <stdio.h>
-
-void zapis(int column, int row, int game_1[row + 2][column + 2]);
-
-#endif
-
